8b5c0e1116c07dbd974bff016a1acbc2642b8ce1
ppphuchen <34165473+ppphuchen@users.noreply.github.com>
2021-07-30 20:58:02 +0800

    Update FaEEnv.py

--- Test/FaEEnv.py
+++ Test/FaEEnv.py
@@ -12,7 +12,7 @@ def test_PoI():
     """
     args = arglists()
     new_PoI = PoI(args)
-    new_PoI.reset(state)
+    new_PoI.reset(state, args)
     new_PoI.exploit_init(uav_agents)
     new_PoI.step(uav_agents)
 def test_UAV():
@@ -22,9 +22,18 @@ def test_UAV():
     """
     args = arglist()
     new_UAV = UAV(args)
-    new_UAV.reset(state)
+    new_UAV.reset(state, args)
     new_UAV.get_observation(distance_from_poi,timestep=100)
-
+def test_Env()
+    """
+    对Env文档进行测试
+    :return: 
+    """
+    args = arglist()
+    new_Env = UAV(args)
+    new_Env.reset(state, args)
+    new_state = new_Env.step(action)
 if __name__ == "__main__"
     test_PoI()
     test_UAV()
+    test_Env()
