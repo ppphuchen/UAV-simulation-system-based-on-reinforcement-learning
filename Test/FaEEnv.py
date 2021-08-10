c8dd08e427a962fde110d07f3a12c0241c9e67f8
ppphuchen <34165473+ppphuchen@users.noreply.github.com>
2021-08-10 16:40:46 +0800

    Update FaEEnv.py

--- Test/FaEEnv.py
+++ Test/FaEEnv.py
@@ -12,28 +12,42 @@ def test_PoI():
     """
     args = arglists()
     new_PoI = PoI(args)
-    new_PoI.reset(state, args)
-    new_PoI.exploit_init(uav_agents)
-    new_PoI.step(uav_agents)
+    state = np.zeros(2)
+    new_PoI.poi_position_reset(state, args)
+
+    uav_agents_pos = np.zeros(100,2)
+    #生成一个100行 2列的uav_pos矩阵 第一列为uav的横坐标，第二列为uav的纵坐标
+    uav_agents_energy = np.zeros(100)
+    #生成一个100个uav的数组，每个uav的能量初始为0
+    new_PoI.exploit_init(uav_agents_pos)
+    new_PoI.step(uav_agents_pos, uav_agents_energy)
 def test_UAV():
     """
     对UAV文档进行测试
     :return:
     """
-    args = arglist()
+    args = arglists()
     new_UAV = UAV(args)
+    state = np.zeros(1,3)
+    #生成一个1行3列的state矩阵，第一列为uav的横坐标，第二列为uav的纵坐标，第三列为uav的角度
     new_UAV.reset(state, args)
+    distance_from_poi = np.zeros(1,100)
+    #生成一个1行100列的矩阵，每一列表示第i个poi距离该uav的距离
     new_UAV.get_observation(distance_from_poi,timestep=100)
-def test_Env()
+def test_Env():
     """
     对Env文档进行测试
     :return: 
     """
-    args = arglist()
+    args = arglists()
     new_Env = UAV(args)
-    new_Env.reset(state, args)
+    new_Env.reset(args)
+    new_Env.agents_reset()
+    new_Env.position_reset()
+    action = np.zeros(100,2)
+    #第一列为当前uav的横坐标，第二列为当前uav的纵坐标
     new_state = new_Env.step(action)
-if __name__ == "__main__"
+if __name__ == "__main__":
+    test_Env()
     test_PoI()
     test_UAV()
-    test_Env()
